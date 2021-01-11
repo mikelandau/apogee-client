e63781ffa2f76c79bcf41381a18fc26f3af5fea5
Mike Landau <mnlandau90@gmail.com>
2021-01-11 15:18:21 -0500

    drew something on screen

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,4 +2,19 @@ import * as PIXI from "pixi.js";
 
 let app = new PIXI.Application({ width: 1280, height: 720 });
 
-document.body.appendChild(app.view);
+document.body.appendChild(app.view); 
+
+const graphics = new PIXI.Graphics();
+
+console.log('hello');
+
+graphics.beginFill(0x0000CC);
+graphics.lineStyle(2, 0xaaaaff, 1);
+graphics.moveTo(200,200);
+graphics.lineTo(250,100);
+graphics.lineTo(300,200);
+graphics.lineTo(250,170);
+graphics.closePath();
+graphics.endFill();
+
+app.stage.addChild(graphics);
---
+++ src/ts/models/fighter.ts
@@ -0,0 +1,11 @@
+export default class Fighter {
+    posX: number;
+    posY: number;
+    orientation: number;
+
+    constructor(posX: number, posY: number, orientation: number) {
+        this.posX = posX;
+        this.posY = posY;
+        this.orientation = orientation;
+    }
+}
