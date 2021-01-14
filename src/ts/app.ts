6553f5d448e079ecd65ea25c0921f861a616109a
Mike Landau <mnlandau90@gmail.com>
2021-01-14 18:44:54 -0500

    added rotation math for fighter

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,20 +1,16 @@
 import * as PIXI from "pixi.js";
+import Game from "./game";
 
 let app = new PIXI.Application({ width: 1280, height: 720 });
 
-document.body.appendChild(app.view); 
+document.body.appendChild(app.view);
 
-const graphics = new PIXI.Graphics();
-
-console.log('hello');
-
-graphics.beginFill(0x0000CC);
-graphics.lineStyle(2, 0xaaaaff, 1);
-graphics.moveTo(200,200);
-graphics.lineTo(250,100);
-graphics.lineTo(300,200);
-graphics.lineTo(250,170);
-graphics.closePath();
-graphics.endFill();
-
-app.stage.addChild(graphics);
+const game = new Game(app.stage);
+game.reset().then(() => {
+    app.ticker.add(async () => {
+        await game.update();
+        await game.draw();
+    });
+}).catch((e) => {
+    console.error(`failed to start game: ${e}`);
+});
---
+++ src/ts/game.ts
@@ -0,0 +1,34 @@
+import Fighter from "./models/fighter";
+
+export default class Game {
+
+    stage: PIXI.Container;
+    graphics: PIXI.Graphics;
+
+    fighter: Fighter;
+
+    constructor(stage: PIXI.Container) {
+        this.stage = stage;
+
+        this.graphics = new PIXI.Graphics();
+        stage.addChild(this.graphics);
+
+        this.fighter = new Fighter();
+    }
+
+    public async reset(): Promise<void> {
+        this.fighter.posX = 200;
+        this.fighter.posY = 200;
+        this.fighter.orientation = 0;
+        this.fighter.velocity = 0;
+    }
+
+    public async update(): Promise<void> {
+        this.fighter.updatePosition();
+    }
+
+    public async draw(): Promise<void> {
+        this.graphics.clear();
+        this.fighter.draw(this.graphics);
+    }
+}
